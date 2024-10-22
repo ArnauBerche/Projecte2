cf1dde97b006012ff707811ee607482b796615d6
Aberche <arnau.berche@enti.cat>
2024-10-22 18:06:17 +0200

    PathfindingUpgrade

--- Insider/Assets/Project/Scripts/EnemyRelated/EnemyPathfinding.cs
+++ Insider/Assets/Project/Scripts/EnemyRelated/EnemyPathfinding.cs
@@ -1,24 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyPathfinding : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform finaltarget;
 
     NavMeshAgent agent;
+    [SerializeField] Transform currentTarget;
+    [SerializeField] int currentTargetNum = 0;
+    [SerializeField] int chosen;
+    [SerializeField] Transform[] path1;
+    [SerializeField] Transform[] path2;
+    [SerializeField] Transform[] pathList;
+
 
     void Start()
     {
+        if (chosen == 0)
+        {
+            pathList = path2;
+        }
+        else 
+        {
+            pathList = path1;
+        }
+ 
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        currentTarget = pathList[currentTargetNum];
     }
 
     void Update()
     {
-        agent.SetDestination(target.position);
+        agent.SetDestination(currentTarget.position);
     }
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform == currentTarget) {
+            currentTargetNum++;
+            if (currentTargetNum == pathList.Length)
+            {
+                currentTarget = finaltarget;
+            }
+            else
+            {
+                currentTarget = pathList[currentTargetNum];
+            }
+
+        }
+    }
 }
